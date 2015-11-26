18cb0442db8d9e3a74aa7a420642adaa260e1dd1
Ludovico Russo <ludus.russo@gmail.com>
2015-11-26 16:20:23 +0100

    add state machine

--- ar_docking/src/ar_docking.cpp
+++ ar_docking/src/ar_docking.cpp
@@ -167,9 +167,9 @@ namespace ar_pose
     if (docking_state_ == HOMING) {
       msg.angular.z = lambda_ * cmd_t + kt * (pos[0]/pos[2]);
       msg.linear.x = lambda_ * cmd_x + kx * (pos[2]);
-      if (pos[2] < 2.2f) docking_state_ = CONNECTING;
+      if (pos[2] < 3.0f) docking_state_ = CONNECTING;
     } else if (docking_state_ == CONNECTING) {
-      msg.linear.x = 0.5f;
+      msg.linear.x = -0.2f;
     } else {
 
     }
